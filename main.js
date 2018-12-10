5f148fba4d95aa60ed0d289dae1651a10d4438b7
unknown <AClark@spartaglobal.com>
2018-12-10 22:20:49 +0000

    Finished trip time & cost calculator

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-var calcSetting = prompt("Enter 'b' for a basic calculator, 'a' for an advanced calculator, and 'bmi' to calculate your BMI.");
+var calcSetting = prompt("Enter 'b' for a basic calculator, 'a' for an advanced calculator, 'bmi' to calculate your BMI, and 'trip' to calculate trip times and price");
 var output = null;
 
 if (calcSetting == "b") {
   var operation = prompt("Please choose the operation you want to perform: +, -, *, /");
-  var numberOne = prompt("Please enter the first number for the calculation");
-  var numberTwo = prompt("Please enter the second number for the calculation");
+  var numberOne = parseFloat(prompt("Please enter the first number for the calculation"));
+  var numberTwo = parseFloat(prompt("Please enter the second number for the calculation"));
 
   switch (operation) {
     case "+":
-      output = parseFloat(numberOne) + parseFloat(numberTwo);
+      output = numberOne + numberTwo;
       break;
 
     case "-":
-      output = parseFloat(numberOne) - parseFloat(numberTwo);
+      output = numberOne - numberTwo;
       break;
 
     case "*":
-      output = parseFloat(numberOne) * parseFloat(numberTwo);
+      output = numberOne * numberTwo;
       break;
 
     case "/":
-      output = parseFloat(numberOne) / parseFloat(numberTwo);
+      output = numberOne / numberTwo;
       break;
 
     default:
@@ -33,17 +33,17 @@ if (calcSetting == "b") {
 
 } else if (calcSetting == "a") {
   var operation = prompt("Please choose the operation you want to perform: sqrt, ^");
-  var numberOne = prompt("Please enter the first number for the calculation");
+  var numberOne = parseFloat(prompt("Please enter the first number for the calculation"));
 
   switch (operation) {
     case "sqrt":
-      output = Math.sqrt(parseFloat(numberOne));
+      output = Math.sqrt(numberOne);
       console.log("The result of the operation: sqrt(" + numberOne + ") = " + output);
       break;
 
     case "^":
-      var numberTwo = prompt("Please enter the second number for the calculation");
-      output = parseFloat(numberOne) ** parseFloat(numberTwo);
+      var numberTwo = parseFloat(prompt("Please enter the second number for the calculation"));
+      output = numberOne ** numberTwo;
       console.log("The result of the operation: " + numberOne + operation + numberTwo + " = " + output);
       break;
 
@@ -53,11 +53,47 @@ if (calcSetting == "b") {
     }
 
 } else if (calcSetting == "bmi") {
-  var imperial = prompt("If you want to use the Metric system, enter '0'. If you want to use the Imperial system, enter '1'.");
-  var mass = prompt("Enter your weight (Metric: Kilograms, Imperial: Pounds)");
-  var height = prompt("Enter your height (Metric: Meters, Imperial: Inches)");
+  var imperial = parseInt(prompt("If you want to use the Metric system, enter '0'. If you want to use the Imperial system, enter '1'."));
+  var mass = parseFloat(prompt("Enter your weight (Metric: Kilograms, Imperial: Pounds)"));
+  var height = parseFloat(prompt("Enter your height (Metric: Meters, Imperial: Inches)"));
 
-  var bmi = (mass / (height**2) ) * (703 * parseInt(imperial));
-  console.log("Your BMI is: " + bmi);
+  var bmi = mass / (height**2);
 
+  if (imperial == 1) {
+    bmi *= 703;
+  }
+
+  var health = null;
+
+  if (bmi < 18.5) {
+    health = "Underweight";
+  } else if (18.5 <= bmi < 25) {
+    health = "Healthy";
+  } else if (25 <= bmi < 30) {
+    health = "Overweight";
+  } else {
+    health = "Obese";
+  }
+
+  console.log("Your BMI is " + bmi + ": " + health);
+
+} else if (calcSetting == "trip") {
+  var distance = parseFloat(prompt("Enter the distance you need to travel (miles): "));
+  var mpg = parseFloat(prompt("Enter the fuel efficiency of your car (miles per gallon): "));
+  var cpg = parseFloat(prompt("Enter the price of fuel (£ per gallon): "));
+  var speed = parseFloat(prompt("Enter the speed you will be travelling (mph): "));
+
+  if (speed > 60) {
+    mpg -= (speed - 60) * 2;
+  }
+
+  if (mpg < 1) {
+    mpg = 1;
+  }
+
+  var time = (distance / speed);
+  var gallons = (distance / mpg);
+  var cost = (gallons * cpg);
+
+  console.log("Your trip will take " + time + " hours and cost £" + cost);
 }
