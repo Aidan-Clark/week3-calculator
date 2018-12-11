2a27056884d023bde36069228c542bad58f42c11
unknown <AClark@spartaglobal.com>
2018-12-11 21:33:20 +0000

    Reworked to loop, use functions, and check user input

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# week3-calculator
+# Week3 Project - Calculator
+
+This calculator was built to achieve 4 things:
+
+- Perform simple operations (Add, Subtract, Multiply, Divide)
+- Perform more advanced operations (Square Root, Powers)
+- Calculate the BMI of an individual
+- Calculate trip times and costs
+
+It uses prompts for user input, and alerts to display messages and results.
--- main.js
+++ main.js
@@ -1,70 +1,118 @@
-var calcSetting = prompt("Enter 'b' for a basic calculator, 'a' for an advanced calculator, 'bmi' to calculate your BMI, and 'trip' to calculate trip times and price");
-var output = null;
-
-if (calcSetting == "b") {
+function basicInput() {
   var operation = prompt("Please choose the operation you want to perform: +, -, *, /");
-  var numberOne = parseFloat(prompt("Please enter the first number for the calculation"));
-  var numberTwo = parseFloat(prompt("Please enter the second number for the calculation"));
+  if (basicOperationCheck(operation)) {
+    var numberOne = prompt("Please enter the first number for the calculation");
+    var numberTwo = prompt("Please enter the second number for the calculation");
 
+    if (isNaN(numberOne) == false && isNaN(numberTwo) == false) {
+      basicCalculator(operation, parseFloat(numberOne), parseFloat(numberTwo));
+    } else {
+      alert("Please enter a valid number");
+    }
+
+  } else {
+    alert("Please enter a valid operation");
+  }
+}
+
+
+function basicOperationCheck(operation) {
   switch (operation) {
     case "+":
-      output = numberOne + numberTwo;
-      break;
-
     case "-":
-      output = numberOne - numberTwo;
-      break;
-
     case "*":
-      output = numberOne * numberTwo;
-      break;
-
     case "/":
-      output = numberOne / numberTwo;
-      break;
+      return true;
 
     default:
-      alert("The character you entered was not expected.");
-      break;
+      return false;
   }
+}
 
-  console.log("The result of the operation: " + numberOne + " " + operation + " " + numberTwo + " = " + output);
+
+function basicCalculator(operation, numberOne, numberTwo) {
+  result = eval(numberOne + operation + numberTwo);
+  alert("The result of the operation: " + numberOne + " " + operation + " " + numberTwo + " = " + result);
+
+}
 
 
-} else if (calcSetting == "a") {
+function advancedInput() {
   var operation = prompt("Please choose the operation you want to perform: sqrt, ^");
-  var numberOne = parseFloat(prompt("Please enter the first number for the calculation"));
 
+  if (advancedOperationCheck(operation)) {
+    var numberOne = prompt("Please enter the first number for the calculation");
+
+    if (isNaN(numberOne) == false) {
+        advancedCalculator(operation, parseFloat(numberOne));
+    } else {
+      alert("Please enter a valid number")
+    }
+
+  } else {
+    alert ("Please enter a valid operation")
+  }
+}
+
+
+function advancedOperationCheck(operation) {
   switch (operation) {
     case "sqrt":
+    case "^":
+      return true;
+
+    default:
+      return false;
+  }
+}
+
+
+function advancedCalculator(operation, numberOne) {
+  switch (operation) {
+
+    case "sqrt":
       output = Math.sqrt(numberOne);
-      console.log("The result of the operation: sqrt(" + numberOne + ") = " + output);
+      alert("The result of the operation: sqrt(" + numberOne + ") = " + output);
       break;
 
     case "^":
-      var numberTwo = parseFloat(prompt("Please enter the second number for the calculation"));
-      output = numberOne ** numberTwo;
-      console.log("The result of the operation: " + numberOne + operation + numberTwo + " = " + output);
-      break;
+      var numberTwo = prompt("Please enter the second number for the calculation");
+      if (isNaN(numberTwo) == false) {
+        output = numberOne ** parseFloat(numberTwo);
+        alert("The result of the operation: " + numberOne + operation + numberTwo + " = " + output);
+      } else {
+        alert("Please enter a valid number");
+      }
 
-    default:
-      alert("The character(s) you entered weren't expected");
       break;
     }
+}
 
-} else if (calcSetting == "bmi") {
-  var imperial = parseInt(prompt("If you want to use the Metric system, enter '0'. If you want to use the Imperial system, enter '1'."));
-  var mass = parseFloat(prompt("Enter your weight (Metric: Kilograms, Imperial: Pounds)"));
-  var height = parseFloat(prompt("Enter your height (Metric: Meters, Imperial: Inches)"));
 
+function bmiInput() {
+  var imperial = prompt("If you want to use the Metric system, enter '0'. If you want to use the Imperial system, enter '1'.");
+  if (imperial == "0" || imperial == "1") {
+    var mass = prompt("Enter your weight (Metric: Kilograms, Imperial: Pounds)");
+    var height = prompt("Enter your height (Metric: Meters, Imperial: Inches)");
+
+    if (isNaN(mass) == false && isNaN(height) == false) {
+      bmiCalculator(parseInt(imperial), parseFloat(mass), parseFloat(height));
+    } else {
+      alert("Please enter a valid number");
+    }
+
+  } else {
+    alert("Please enter either a 1 or 0.")
+  }
+}
+
+
+function bmiCalculator(imperial, mass, height) {
   var bmi = mass / (height**2);
-
   if (imperial == 1) {
     bmi *= 703;
   }
-
   var health = null;
-
   if (bmi < 18.5) {
     health = "Underweight";
   } else if (18.5 <= bmi < 25) {
@@ -74,15 +122,24 @@ if (calcSetting == "b") {
   } else {
     health = "Obese";
   }
+  alert("Your BMI is " + bmi + ": " + health);
+}
 
-  console.log("Your BMI is " + bmi + ": " + health);
+function tripInput() {
+  var distance = prompt("Enter the distance you need to travel (miles): ");
+  var mpg = prompt("Enter the fuel efficiency of your car (miles per gallon): ");
+  var cpg = prompt("Enter the price of fuel (£ per gallon): ");
+  var speed = prompt("Enter the speed you will be travelling (mph): ");
 
-} else if (calcSetting == "trip") {
-  var distance = parseFloat(prompt("Enter the distance you need to travel (miles): "));
-  var mpg = parseFloat(prompt("Enter the fuel efficiency of your car (miles per gallon): "));
-  var cpg = parseFloat(prompt("Enter the price of fuel (£ per gallon): "));
-  var speed = parseFloat(prompt("Enter the speed you will be travelling (mph): "));
+  if (isNaN(distance) == false && isNaN(mpg) == false && isNaN(cpg) == false && isNaN(speed) == false) {
+    tripCalculation(parseFloat(distance), parseFloat(mpg), parseFloat(cpg), parseFloat(speed));
+  } else {
+    alert("Please enter a valid number.");
+  }
+}
 
+
+function tripCalculation(distance, mpg, cpg, speed) {
   if (speed > 60) {
     mpg -= (speed - 60) * 2;
   }
@@ -95,5 +152,31 @@ if (calcSetting == "b") {
   var gallons = (distance / mpg);
   var cost = (gallons * cpg);
 
-  console.log("Your trip will take " + time + " hours and cost £" + cost);
+  alert("Your trip will take " + time + " hours and cost £" + cost);
+}
+
+keepRunning = true;
+
+while (keepRunning == true) {
+  var calcSetting = prompt("Enter 'b' for a basic calculator, 'a' for an advanced calculator, 'bmi' to calculate your BMI, 'trip' to calculate trip times and price, and 'x' to close the program");
+  var output = null;
+
+  if (calcSetting == "b") {
+    basicInput();
+
+  } else if (calcSetting == "a") {
+    advancedInput();
+
+  } else if (calcSetting == "bmi") {
+    bmiInput();
+
+  } else if (calcSetting == "trip") {
+    tripInput();
+
+  } else if (calcSetting == "x") {
+    keepRunning = false;
+
+  } else {
+    alert("Please enter a correct input.")
+  }
 }
